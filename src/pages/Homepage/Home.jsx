fd82888b9d55db53c6cabb729ad0fc83d1b89e45
RodrigoNeoEx <draja.acc@gmail.com>
2021-07-15 19:22:19 -0300

    add animation on home

--- src/pages/Homepage/Home.jsx
+++ src/pages/Homepage/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import ButterFly from '../../components/ButterFly/Butterfly';
 import Header from '../../components/Header/Header';
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <ButterFly />
     </div>
   )
 }
