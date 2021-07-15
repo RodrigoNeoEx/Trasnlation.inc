d5e707d681dc3a159becb0e3235243e62ca42980
RodrigoNeoEx <draja.acc@gmail.com>
2021-07-15 19:59:49 -0300

    project done, jus left readme

--- src/pages/Homepage/Home.jsx
+++ src/pages/Homepage/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ButterFly from '../../components/ButterFly/Butterfly';
 import Header from '../../components/Header/Header';
+import './style/homepage.css'
 
 const Home = () => {
   return (
     <div>
       <Header />
       <ButterFly />
+      <p className="home__description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus, laudantium fugiat. Neque mollitia nihil molestiae saepe quia? Eveniet vitae, vel quos molestiae eos debitis obcaecati tenetur labore deserunt beatae illum?</p>
     </div>
   )
 }
