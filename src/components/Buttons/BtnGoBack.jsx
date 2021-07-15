66c29e9a8e034ede50c4ff3c129e1e465220b4c0
RodrigoNeoEx <draja.acc@gmail.com>
2021-07-15 11:22:59 -0300

    css btnBack mobile first

--- src/components/Buttons/BtnGoBack.jsx
+++ src/components/Buttons/BtnGoBack.jsx
@@ -1,9 +1,20 @@
 import React from"react";
 import { useHistory } from "react-router";
+import { TiArrowBack } from "react-icons/ti"
+import './styles/btnGoBack.css'
+
+
 
 const BtnGoBack = () => {
   const history = useHistory()
-  return <button onClick={history.goBack}>back</button>
+  return (
+    <button
+      onClick={history.goBack}
+      className="btnBack"
+    >
+      <TiArrowBack />
+    </button>
+  )
 }
 
 export default BtnGoBack;
