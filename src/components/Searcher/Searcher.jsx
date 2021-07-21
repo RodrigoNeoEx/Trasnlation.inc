3f800f8b5a562bc5f688cc3c197e9eb4257d1384
RodrigoNeoEx <draja.acc@gmail.com>
2021-07-21 18:12:44 -0300

    removed unused imports

--- src/components/Searcher/Searcher.jsx
+++ src/components/Searcher/Searcher.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory, useLocation } from 'react-router-dom';
 import { getSearch } from '../../redux/slicers/searchSlice';
